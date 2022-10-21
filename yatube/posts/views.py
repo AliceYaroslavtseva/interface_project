73544b3370f5b1a50d44e664aa455586c78247b6
Алиса Ярославцева <yaroslavtsevaalisa@yandex.ru>
2022-10-21 10:08:29 +0400

    Project now pep8-4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -115,7 +115,8 @@ def follow_index(request):
 def profile_follow(request, username):
     user = request.user
     author = get_object_or_404(User, username=username)
-    if user != author:
+    is_follower = Follow.objects.filter(user=user, author=author)
+    if user != author and not is_follower.exists():
         Follow.objects.create(user=user, author=author)
     return redirect('posts:follow_index')
 
